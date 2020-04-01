6cde3412f0abb2bf2f9220856c71683fe145f806
VICENTE NASCIMENTO <vicente19981@live.com>
2020-04-01 15:31:54 -0300

    Front End e Back end Da Aplicação

---
+++ db.js
@@ -0,0 +1,22 @@
+const sqlite3 = require('sqlite3').verbose()
+
+const db = new sqlite3.Database('./workshop.db');
+
+
+db.serialize(function() { 
+    //Criar a tabela
+    db.run(`
+        CREATE TABLE IF NOT EXISTS ideas(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            image TEXT,
+            title TEXT,
+            category TEXT,
+            description TEXT,
+            link TEXT  
+        ); 
+    `)
+
+})
+
+
+module.exports = db;
